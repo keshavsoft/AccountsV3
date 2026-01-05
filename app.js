6db681a6079cb65fe6a3535ad0696a14311e9e82
keshavsoft <nknnkumar@yahoo.com>
2026-01-05 16:42:44 +0530

    KeshavSoft

---
+++ app.js
@@ -0,0 +1,40 @@
+import express from 'express';
+import cookieParser from 'cookie-parser';
+
+const app = express();
+
+var port = normalizePort(process.env.PORT || 3000);
+
+import { StartFunc as StartFuncFromMiddleware } from "./Token/MiddleWares/entryFile.js";
+
+import { router as routerFromUtility } from "./Utility/routes.js";
+import { router as routerFromSecret } from "./Secret/routes.js";
+import { router as routerFromUsers } from "./Users/routes.js";
+import { router as routerFromSV1 } from "./SV1/routes.js";
+
+app.use(express.static('Public'));
+app.use(cookieParser());
+
+app.use("/Utility", routerFromUtility);
+app.use("/Secret", routerFromSecret);
+app.use("/Users", routerFromUsers);
+app.use("/SV1", StartFuncFromMiddleware, routerFromSV1);
+
+function normalizePort(val) {
+    var port = parseInt(val, 10);
+
+    if (isNaN(port)) {
+        return val;
+    }
+
+    if (port >= 0) {
+        return port;
+    }
+
+    return false;
+};
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+    console.log(`Open here http://localhost:${port}`);
+});
